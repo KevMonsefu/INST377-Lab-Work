08c1ee422da7d36b61bd718d5779b25c5be0ce3e
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-02-18 18:33:55 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const squares = Array.from(document.querySelectorAll('.grid div'));
   const ScoreDisplay = document.querySelector('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10;
 
   // Tetrominoes
   const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
+    [1,width + 1,width * 2 + 1,2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
