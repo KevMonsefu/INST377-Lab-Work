84220bf8de7942d30f548555c048694c69435917
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-02-18 17:41:58 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -11,66 +11,66 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2]
-  ];
+  ]
 
   const zTetromino = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1]
-  ];
+  ]
 
   const tTetromino = [
     [1, width, width + 1, width + 2],
     [1, width + 1, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1]
-  ];
+  ]
 
   const oTetromino = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1]
-  ];
+  ]
 
   const iTetromino = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3]
-  ];
+  ]
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
-  let currentPosition = 2;
-  let currentRotation = 0;
+  let currentPosition = 4
+  let currentRotation = 0
 
   // Random select tetromino
-  let random = Math.floor(Math.random()*theTetrominoes.length);
-  let current = theTetrominoes[random][0];
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][0]
 
   // 1st tetromino rotation
   function draw() {
     current.forEach(index => {
       squares[currentPosition = index].classList.add('tetromino');
-    });
+    })
   };
 
   // Undraw Tetromino
   function undraw() {
     current.forEach(index => {
       squares[currentPosition = index].classList.remove('tetromino');
-    });
-  };
+    })
+  }
 
   // Make tetromino move down every second
-  timerId = setInterval(moveDown, 1000);
+  timerId = setInterval(moveDown, 1000)
 
   // Move down function
   function moveDown() {
-    undraw();
-    currentPosition += width;
-    draw();
+    undraw()
+    currentPosition += width
+    draw()
   }
-});
+})
