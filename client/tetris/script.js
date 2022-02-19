1568635ffaf115e7a674796bc5bebfde9ede118a
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-02-18 19:21:31 -0500

    commit

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const startBtn = document.querySelector('#start-button')
   const width = 10
 
-  //The Tetrominoes
+  // The Tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
