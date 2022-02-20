c42fb88484f0e7fbf9dd6a51489f350902e1707c
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-02-20 17:03:04 -0500

    commit

---
+++ client/lab_4/script.js
@@ -0,0 +1,14 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('photo-grid-item');
+const totalSlides = slides.length;
+
+document.
+  getElementById('carousel__button--next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('carousel__button--prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
