6732e28dbcafa809091ad903c78b567a1e2cbe0b
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-03-14 01:13:17 -0400

    commit (watched video)

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,15 +1,36 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.cell(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + min);
+}
+
+function dataHandeler(dataArray) {
+  console.table(arrayFromJson); // this is called "dot notation"
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
+  });
+}
 // Ass the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
   const form = document.querySelector('.container'); // change this selector to match the id or classname of your actual form
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+  const submit = document.querySelector('.submit_button'); // change this selector to match the id or classname of your actual form
+  submit.style.display = 'block';
+
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  cconsole.log(arrayFromJson);
+  if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a "breakpoint"
+      // arrayFromJson.data - we're accessing a key called 'data' on the returned object
+      // it contains all 1,000 records we need
+      dataHandeler(arrayFromJson.data);
+    });
+  }
 }
 
 // this actually runs first! It's calling the function above
