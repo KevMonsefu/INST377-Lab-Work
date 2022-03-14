8860f422fa48b2ac7ac930c7a4745cfc50d76aba
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-03-14 01:35:50 -0400

    commiut

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -8,7 +8,7 @@ function dataHandeler(dataArray) {
   console.log('fired dataHandler');
   console.table(arrayFromJson); // this is called "dot notation"
   const range = [...Array(15).keys()];
-  const listItems = range.map((item,index) => {
+  const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
   });
@@ -28,10 +28,11 @@ async function mainEvent() { // the async keyword means we can make API requests
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   console.log(arrayFromJson);
   if (arrayFromJson.data.length > 0) {
-    submit.style.display = 'block';
+    console.log('I made it here');
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       console.log('form submission'); // this is substituting for a "breakpoint"
+      submit.style.display = 'block';
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
       dataHandeler(arrayFromJson.data);
