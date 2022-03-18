026034ac8ea5a7b7692f1de615137259696ce7a1
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-03-18 17:19:09 -0400

    math fix

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,5 +1,5 @@
 function getRandomIntInclusive(min, max) {
-  const newMin = Math.cell(min);
+  const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin + 1) + newMIN);
 }
